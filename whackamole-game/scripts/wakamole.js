17ac65c618b0d434712328ed526120388a5b6642
Is an English Major <Najeebihesa@gmail.com>
2025-07-09 13:59:45 +0300

    add click handler and score

--- whackamole-game/scripts/wakamole.js
+++ whackamole-game/scripts/wakamole.js
@@ -1,17 +1,53 @@
 function init (){
 //everything goes here
 const gridElm = document.querySelector('.grid')
+const cells =[]
+const gridWidth = 10
+const numberOfCells = gridWidth * gridWidth
+const scoreElm = document.querySelector('#score-display')
+let duckPos = 47
+let score = 0
+function addDuck(){
+  cells[duckPos].classList.add('duck')
+}
 
+function removeDuck(){
+  cells[duckPos].classList.remove('duck')
+}
+function play(){
+  //writing an arrow func 
+  setInterval(()=>{
+    removeDuck()
+    duckPos = Math.floor(Math.random()* numberOfCells)
+    addDuck()
+    //3000ms is 3s
+  }, 3000)
+}
+function handleClick(event){
+  if (event.target.classList.contains('duck')){
+    score += 10
+    scoreElm.textContent =`Your score is ${score}`
+    console.log(score)
+  }
+}
 function createGrid(){
     //every cell i want - make a div
     //append this cell to my grid
 
-    for (let i=0; i < 100; i++){
+    for (let i=0; i < numberOfCells; i++){
     const cell = document.createElement('div')
-    gridElm.appendChild(cell)
+    //cell.classList.add('duck')
     cell.textContent = i
+    cell.addEventListener('click', handleClick)
+    cells.push(cell)
+    gridElm.appendChild(cell)
     }
+    console.log(cells)
   }
   createGrid();
+  play()
+
+
 }
+
 document.addEventListener('DOMContentLoaded', init) 
